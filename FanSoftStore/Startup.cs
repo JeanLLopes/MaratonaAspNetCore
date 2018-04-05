a347d49c5d6203caf31686b4164aa3ec74955c11
Jean Lopes <jeean.llopes@outlook.com>
2018-04-05 00:12:36 -0300

    udpate

--- FanSoftStore/Startup.cs
+++ FanSoftStore/Startup.cs
@@ -17,7 +17,7 @@ namespace FanSoftStore
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.dasd
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
