ec3e79112826eae93f9a211dda563872e6505100
Jean Lopes <jeean.llopes@outlook.com>
2018-04-05 00:35:09 -0300

    sad

--- FanSoftStore/Startup.cs
+++ FanSoftStore/Startup.cs
@@ -17,7 +17,7 @@ namespace FanSoftStore
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline....
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
