0df7e3411447e4ef4586d2d517b6202074b678ae
jlopes <jean.llopes@outlook.com>
2018-04-06 00:50:17 -0300

    Add Views Add Models Add Controllerts

---
+++ FanSoftStore/Controllers/ProdutosController.cs
@@ -0,0 +1,25 @@
+﻿using FanSoftStore.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FanSoftStore.UI.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new List<ProdutoModel>()
+            {
+                new ProdutoModel(){Nome = "Picanha", Tipo = "Alimento", Valor = 80.90},
+                new ProdutoModel(){Nome = "Paste de Dente", Tipo = "Higiene", Valor = 2.90},
+                new ProdutoModel(){Nome = "Leite", Tipo = "Alimento", Valor = 1.90}
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ FanSoftStore/Models/ProdutoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FanSoftStore.UI.Models
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public double Valor { get; set; }
+    }
+}
