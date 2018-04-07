b41820cf2483000191bfe4a2039a8f8e7049fcd2
jlopes <jean.llopes@outlook.com>
2018-04-07 00:20:48 -0300

    Add Connection

---
+++ FanSoftStore/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using FanSoftStore.UI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FanSoftStore.UI.Data
+{
+    public class DataContext : DbContext
+    {
+        //GET THE STRING CONNECTION
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DB_FANSOFTSTORE;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        //MAP THE TABLE
+        public DbSet<ProdutoModel> Produtos { get; set; }
+    }
+}
