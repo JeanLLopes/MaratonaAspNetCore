0fe5a54b46bddfcf2b3ba8423df75d59952429e3
jlopes <jean.llopes@outlook.com>
2018-04-08 22:50:33 -0300

    Fixed bug

--- FanSoftStore/Infra/ApplicationBuilderExtensions.cs
+++ FanSoftStore/Infra/ApplicationBuilderExtensions.cs
@@ -18,8 +18,7 @@ namespace FanSoftStore.UI.Infra
             option.RequestPath = "/node_modules";
             var path = Path.Combine(rootFile, "node_modules");
             option.FileProvider = new PhysicalFileProvider(path);
-
-            app.UseStaticFiles();
+            app.UseStaticFiles(option);
             return app;
         }
     }
